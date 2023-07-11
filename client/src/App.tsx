df9dbe6f922b0c14126af63604bd59b50c0e12ab
Bezawit <bezawit.teklemariam@gmail.com>
2023-07-11 16:57:32 +0200

    run client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import HomePage from "./pages/HomePage";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
