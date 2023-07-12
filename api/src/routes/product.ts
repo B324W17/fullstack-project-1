c2a0cbdcca27aa03d99010b5eb03138c6d1c342c
Bezawit <bezawit.teklemariam@gmail.com>
2023-07-13 00:26:06 +0200

    check order in db

--- api/src/routes/product.ts
+++ api/src/routes/product.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import { createProduct, getAllProducts, getProductById} from "./../controllers/products";
+import { createOrder } from "../controllers/orders";
 
 const router = Router();
 
 router.post("/", createProduct);
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
+router.post("/orders", createOrder);
 
 export default router;
