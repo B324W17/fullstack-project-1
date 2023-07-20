789f4637057f0bb519450b2d61c8c36ae58bb6b1
Bezawit <bezawit.teklemariam@gmail.com>
2023-07-20 23:41:53 +0200

    create order from FE

--- client/src/components/cart/CartList.tsx
+++ client/src/components/cart/CartList.tsx
@@ -2,13 +2,30 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import CartItem from "./CartItem";
 import { Button } from "@mui/material";
+import axios from "axios";
 
 export default function CartList() {
   const cartList = useSelector((state: RootState) => state.cart.cartItems);
   const totalValue = useSelector((state: RootState) => state.cart.total);
+  const userData = useSelector((state: RootState) => state.user.userData);
 
-    function handleCheckOut() { }
-    
+  const userId = userData?._id;
+
+  function handleCheckOut() {
+    const endpoint = "http://localhost:7000/orders";
+
+    const products = cartList.map((item) => ({
+      title: item.product.title,
+      price: item.product.price,
+      quantity: item.quantity,
+    }));
+    axios
+      .post(endpoint, { userId: userId, products: products })
+      .then((response) => console.log(response.data))
+      .catch((error) => error);
+  }
+
+  console.log(cartList);
   if (cartList.length === 0) {
     return (
       <div>
