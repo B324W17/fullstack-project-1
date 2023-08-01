bb706f5a839a06a306bc81adcb325dadba6dc163
Bezawit <bezawit.teklemariam@gmail.com>
2023-08-01 23:15:52 +0200

    calculate total using pre-hook

--- api/src/models/Order.ts
+++ api/src/models/Order.ts
@@ -37,7 +37,19 @@ const OrderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, //from user document
   total: {
     type: Number,
+    default: 0, //set default value as 0 for the total field
   },
+});
+
+//Pre-hook to update the total field when a new order document is saved
+OrderSchema.pre<OrderDocument>("save", function (next) {
+  const total = this.products.reduce(
+    (accumulator, product) => accumulator + product.price * product.quantity,
+    0
+  );
+
+  this.total = total;
+  next();
 });
 
 export default mongoose.model<OrderDocument>("Order", OrderSchema);
